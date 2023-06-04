8afc9b84812f9e243eea3a4a86b01cbcda7aabba
Toan Duong <toandt@nexlesoft.com>
2023-06-04 23:04:09 +0700

    tam dc/ suy nghi them y tuong di

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Dashboard = (props) => {
-    const { data } = props
-    const { comment, nickname, profilePictureUrl } = data;
-    console.log(data);
-    return (
-      <div>
-        <div>{}</div>
-      </div>
-    );
-}
+  const { data } = props;
+  const { comment, nickname, profilePictureUrl } = data;
+  console.log(data);
+  return (
+    <div>
+      <img src={profilePictureUrl} />
+      <div>{nickname}</div>
+      <div>{comment}</div>
+    </div>
+  );
+};
 
 export default Dashboard;
